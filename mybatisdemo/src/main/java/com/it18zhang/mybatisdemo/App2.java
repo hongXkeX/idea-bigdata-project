1ff4225de56d222295e971f449ecf9c3e3f687d8
hongXkeX <wyhongke@163.com>
2017-10-31 09:46:23 +0800

    yuan first

---
+++ mybatisdemo/src/main/java/com/it18zhang/mybatisdemo/App2.java
@@ -0,0 +1,11 @@
+package com.it18zhang.mybatisdemo;
+
+/**
+ * @author yuan
+ * @date 2017/10/31 9:44
+ */
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!");
+    }
+}
