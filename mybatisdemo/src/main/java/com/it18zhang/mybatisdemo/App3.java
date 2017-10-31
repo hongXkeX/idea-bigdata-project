732804f3e42511fc5a3df0d7f8b394f105a2f8f7
hongXkeX <wyhongke@163.com>
2017-10-31 10:07:54 +0800

    second

--- mybatisdemo/src/main/java/com/it18zhang/mybatisdemo/App3.java
+++ mybatisdemo/src/main/java/com/it18zhang/mybatisdemo/App3.java
@@ -7,5 +7,6 @@ package com.it18zhang.mybatisdemo;
 public class App3 {
     public static void main(String[] args) {
         System.out.println("Hello World333333333");
+        System.out.println("Hello World444444444");
     }
 }
